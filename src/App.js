9a48ede984671dac184c3b62bee773965c7d9e1a
ChristianHopf <cehopf@gmail.com>
2022-03-26 00:27:39 -0400

    Added * path to App.js routing

--- src/App.js
+++ src/App.js
@@ -11,6 +11,7 @@ function App() {
         <Routes>
           <Route path="/" element={<Home/>}/>
           <Route path="/books" element={<Bookreviews/>}/>
+          <Route path="*" element={<Home/>}/>
         </Routes>
       </Router>
     </div>
