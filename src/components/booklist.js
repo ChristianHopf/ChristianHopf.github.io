049dd90dc657eca039719cbf7d2c3bb64faf049f
ChristianHopf <cehopf@gmail.com>
2022-03-28 23:17:06 -0400

    Prettified book reviews page

--- src/components/booklist.js
+++ src/components/booklist.js
@@ -2,49 +2,51 @@ import '../App.css';
 import axios from 'axios';
 import React from 'react';
 
-function RatingColor(rating){
+function RatingColor(rating) {
     var r = parseInt(rating);
-    if(r < 3.33)
+    if (r < 3.33)
         return 'red';
-    if(r < 6.66)
+    if (r < 6.66)
         return 'yellow';
     return 'green';
 }
 
-function Booklist(){
+function Booklist() {
     // get JSON of all books, parse and map into book components
     const url = 'https://9or9d5fu9g.execute-api.us-east-1.amazonaws.com/Stage/api/books';
     const [books, setBooks] = React.useState([]);
     React.useEffect(() => {
         axios.get(url)
-            .then(function (response){
+            .then(function (response) {
                 const booklist = JSON.parse(response.data.body);
                 setBooks(booklist);
             })
-            .catch(function (error){
+            .catch(function (error) {
                 console.error(error.response);
             });
     }, []);
 
-    const booksElements = books.map((book) =>  
-        <div className="book">
-            <img id="bookimg" src={require(`../images/${book.Imagefile.S}`)} alt={`Cover art for ${book.Title.S}`}></img>
+    const booksElements = books.map((book) =>
+        <div className="book" key={book.Imagefile.S}>
+            <img className="bookimg" src={require(`../images/${book.Imagefile.S}`)} alt={`Cover art for ${book.Title.S}`}></img>
             <div className="bookinfo">
                 <div className="bookheader">
-                    <h3>{book.Title.S}</h3>
-                    <p>{book.Author.S}, {book.Genre.S}</p>
+                    <div className="bookheader-info">
+                        <h3>{book.Title.S}</h3>
+                        <p>{book.Author.S}, {book.Genre.S}</p>
+                    </div>
+                    <div>
+                        <span className={`bookrating-${RatingColor(book.Rating.N)}`}>{book.Rating.N}/10</span>
+                    </div>
                 </div>
                 <div className="bookparagraph">
                     {book.Review.S}
-                </div>
-                <div className={`bookrating-${RatingColor(book.Rating.N)}`}>
-                    Rating: {book.Rating.N}/10
                 </div>
             </div>
         </div>
     );
 
-    return(
+    return (
         <div className="bookreviewsbody">
             {booksElements}
         </div>
---
+++ src/components/projectnavigation.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+function Projectnavigation(){
+
+}
+
+export default Projectnavigation;
